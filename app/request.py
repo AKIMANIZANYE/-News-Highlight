8fe9240e8b93068c11281d30eb48d3a58b84cb2b
Claudine Akimanizanye <akimanizanye.claudine@gmail.com>
2019-02-19 14:20:53 +0200

    I  created object in request .py

--- app/request.py
+++ app/request.py
@@ -4,7 +4,7 @@ from .Models import News
 
 News=News.News
 
-# api_key = app.config['News_API_KEY']
+ api_key = app.config['News_API_KEY']
 
 # Getting the movie base url
 base_url = app.config["NEWS_API_BASE_URL"]
@@ -20,17 +20,17 @@ def get_news(category):
         with urllib.request.urlopen(get_news_url) as url:
                 get_news_data = url.read()
                 get_news_response = json.loads(get_news_data)
-                news_results = None
+                news_articles = None
 
                 if get_news_response['articles']:
-                        news_results_list = get_news_response['articles']
-                        news_results = process_results(news_results_list)
+                        news_articles_list = get_news_response['articles']
+                        news_articles = process_articles(news_articles_list)
         
-        return news_results
+        return news_articles
 
-def process_results(news_list):
+def process_articles(news_list):
     
-        news_results = []
+        news_articles= []
         for news_item in news_list:
                 
                 id = news_item.get('id')
@@ -45,31 +45,31 @@ def process_results(news_list):
 
                 if poster:
                         news_object = News(news_id,title,name,author,description,url, urlToImage,publishedAt)
-                        news_results.append(news_object)
+                        news_articles.append(news_object)
 
-        return news_results
+        return news_articles
 
 def get_new(id):
         get_new_details_url = base_url.format(id,api_key)
 
-        # with urllib.request.urlopen(get_new_details_url) as url:
-        #         new_details_data = url.read()
-        #         new_details_response = json.loads(new_details_data)
+        with urllib.request.urlopen(get_new_details_url) as url:
+                new_details_data = url.read()
+                new_details_response = json.loads(new_details_data)
 
-        #         new_object = None
-        #         if new_details_response:
-        #                 id = news_details_response.get('id')
-        #                 title = news_details_response.get('title')
-        #                 name = news_details_response.get('name')
-        #                 author=news_details_response.get('author')
-        #                 description=news_details_response.get('description')
-        #                 url=news_details_response.get('link')
-        #                 urlToImage=news_details_response.get('image')
-        #                 publishedAt=news_details_response.get('date')
-        #                 poster = news_details_response.get('poster_path')
-        #                 new_object = News(id,title,name,author,description,url,urlToImage,publishedAt,poster)
+                new_object = None
+                if new_details_response:
+                        id = news_details_response.get('id')
+                        title = news_details_response.get('title')
+                        name = news_details_response.get('name')
+                        author=news_details_response.get('author')
+                        description=news_details_response.get('description')
+                        url=news_details_response.get('url)
+                        urlToImage=news_details_response.get(' urlToImage')
+                        publishedAt =news_details_response.get('publishedAt')
+                        poster = news_details_response.get('poster')
+                        new_object = News(id,title,name,author,description,url,urlToImage,publishedAt,poster)
 
-        # return new_object
+        return new_object
 def search_new(new_name):
         search_new_url = 'https://newsapi.org/v2/everything?q=bitcoin&from=2019-01-18&sortBy=publishedAt&apiKey=870c6f91cc3244ac9013dcbecb84e54d?api_key={}&query={}'.format(api_key,new_name)
         # with urllib.request.urlopen(search_new_url) as url:
